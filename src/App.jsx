cc744fc0542ecaac21f18236c287bbaef253961a
Camilo Beltran <camilobeltran@macbookpro.lan>
2025-06-26 21:55:29 -0500

    Initial commit: Set up project with Vite, React, Tailwind and Framer Motion

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white text-center">
+        ¡Proyecto Configurado!
+        <br />
+        <span className="text-2xl text-cyan-400">
+          React + Vite + Tailwind CSS
+        </span>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
